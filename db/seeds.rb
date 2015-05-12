5183f22f625b132c00c0416368f962863c169aa7
Gianpietro Lavado <gianpietro1@gmail.com>
2015-05-12 14:59:00 -0500

    basic seed file for radios & admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,11 +14,11 @@ Radio.create(name: 'Estación 3', description: 'tropical', player: "http://playe
 Radio.create(name: 'Estación 4', description: 'rock, pop y afines', player: "http://player.radiocdn.com/iframe.js?hash=5e6ead35300119bc5234f1800b455a948ddedf3d-450-135", host: "eclipse.wavestreaming.com", port: 3771)
 Radio.create(name: 'Estación 5', description: 'instrumental', player: "http://player.radiocdn.com/iframe.js?hash=5e6ead35300119bc5234f1800b455a948ddedf3d-450-135", host: "eclipse.wavestreaming.com", port: 3771)
 
-Radio.find_by(id: 1),update_attributes(name: 'station 1', description: 'coast & folk', locale: :en)
-Radio.find_by(id: 2),update_attributes(name: 'station 2', description: 'andean', locale: :en)
-Radio.find_by(id: 3),update_attributes(name: 'station 3', description: 'tropical', locale: :en)
-Radio.find_by(id: 4),update_attributes(name: 'station 4', description: 'rock, pop & related', locale: :en)
-Radio.find_by(id: 5),update_attributes(name: 'station 5', description: 'instrumental', locale: :en)
+Radio.find_by(id: 1).update_attributes(name: 'station 1', description: 'coast & folk', locale: :en)
+Radio.find_by(id: 2).update_attributes(name: 'station 2', description: 'andean', locale: :en)
+Radio.find_by(id: 3).update_attributes(name: 'station 3', description: 'tropical', locale: :en)
+Radio.find_by(id: 4).update_attributes(name: 'station 4', description: 'rock, pop & related', locale: :en)
+Radio.find_by(id: 5).update_attributes(name: 'station 5', description: 'instrumental', locale: :en)
 
 # Admin user
 
