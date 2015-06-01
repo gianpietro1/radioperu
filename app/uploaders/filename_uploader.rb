31203aeaa6eeabd500e3e134f0a8839d5514b053
Gianpietro Lavado <gianpietro1@gmail.com>
2015-05-31 22:46:40 -0500

    conditional, fog for prod mp3, local for dev

--- app/uploaders/filename_uploader.rb
+++ app/uploaders/filename_uploader.rb
@@ -8,8 +8,11 @@ class FilenameUploader < CarrierWave::Uploader::Base
   include CarrierWave::MimeTypes
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.development?
+    storage :file
+  elsif Rails.env.production?
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
