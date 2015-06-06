72536583a8c447b2fd8218aa0f1fd6d1adb5dd30
Gianpietro Lavado <gianpietro1@gmail.com>
2015-06-06 00:02:41 -0500

    id3 populating fields

--- db/migrate/20150606040513_add_default_to_id3.rb
+++ db/migrate/20150606040513_add_default_to_id3.rb
@@ -1,7 +1,7 @@
 class AddDefaultToId3 < ActiveRecord::Migration
   def change
     rename_column :songs, :length, :id3
-    change_column :songs, :id3, :boolean
+    change_column :songs, :id3, 'boolean USING CAST(id3 AS boolean)'
     change_column_default(:songs, :id3, false)
   end
 end
