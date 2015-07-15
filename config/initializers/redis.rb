9f2443e317755121319ea448dc52d7fe7b786e15
Gianpietro Lavado <gianpietro1@gmail.com>
2015-07-15 00:47:50 -0500

    production testing

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 if Rails.env.development? || Rails.env.test?
  $redis = Redis.new(:host => 'localhost', :port => 6379)
 elsif Rails.env.production?
- #$redis = Redis.new(:host => 'scat.redistogo.com', :port => 10384, :password => ENV['REDISTOGO_PASSWORD'])
+ $redis = Redis.new(:host => 'localhost', :port => 6379)
 end
