2d5daeab0b5e14aff6afc8fa21d8d0accfcfc632
Gianpietro Lavado <gianpietro1@gmail.com>
2015-07-16 15:14:15 -0500

    prepare for prod & test

--- app/uploaders/banner_uploader.rb
+++ app/uploaders/banner_uploader.rb
@@ -13,7 +13,7 @@ class BannerUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "/uploads/images/banners"
+    "uploads/images/banners"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
