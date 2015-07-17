4f1a64868155b9e8fc969a76276e135a2b9b7027
Gianpietro Lavado <gianpietro1@gmail.com>
2015-07-17 14:22:15 -0500

    more responsiveness

--- app/controllers/search_suggestions_controller.rb
+++ app/controllers/search_suggestions_controller.rb
@@ -9,7 +9,7 @@ class SearchSuggestionsController < ApplicationController
       @search_array << {label: album.name + " - " + album.artist.name, category: t(:albums), model: "Album", model_id: album.id, parent_model_id: album.artist.id}
     end
     Song.search_by_name(params[:term]).map do |song|
-      @search_array << {label: song.name + " - " + song.album.artist.name, category: t(:songs), model: "Song", model_id: song.id, parent_model_id: song.album.id, gparent_model_id: song.album.artist.id}
+      @search_array << {label: song.name + " - " + song.album.name + " - " + song.album.artist.name, category: t(:songs), model: "Song", model_id: song.id, parent_model_id: song.album.id, gparent_model_id: song.album.artist.id}
     end
     render json: @search_array
   end
