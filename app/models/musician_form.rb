6c03508226c6c574bde4721f8c24b66e0cea17ad
Gianpietro Lavado <gianpietro1@gmail.com>
2015-07-27 21:16:55 -0500

    style and musician form comments

--- app/models/musician_form.rb
+++ app/models/musician_form.rb
@@ -4,6 +4,7 @@ class MusicianForm < MailForm::Base
   attribute :musician_phone,     :validate => false      
   attribute :musician_email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :musician_url,       :validate => true
+  attribute :musician_comments,       :validate => false
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
