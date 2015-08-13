165599b2213cd3890a3d8dcfb23934d8810795af
Gianpietro Lavado <gianpietro1@gmail.com>
2015-08-13 14:00:10 -0500

    automate user creation & assignment / corrected translations

--- lib/usercontrol.rb
+++ lib/usercontrol.rb
@@ -1,7 +1,7 @@
 class Usercontrol
 
-  def self.create(email,password,name,artist)
-    User.create!(
+  def self.createall(email,password,name,artist)
+    user = User.create!(
       email: email, password: password, reset_password_token: nil, 
         reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, 
         current_sign_in_at: Time.now, last_sign_in_at: Time.now, 
@@ -10,23 +10,25 @@ class Usercontrol
         name: name, username: nil, role: 'musician'
     )
 
-    user = User.find_by(email: email).id
-
     Artist.create!(
       name: artist,
-      user_id: user
+      user_id: user.id
       )
 
   end
 
-  def self.assign(email,artist)
+  def self.createuser(email,password,name,artist)
 
-    user = User.find_by(email: email).id
+    user = User.create!(
+      email: email, password: password, reset_password_token: nil, 
+        reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, 
+        current_sign_in_at: Time.now, last_sign_in_at: Time.now, 
+        current_sign_in_ip: "::1", last_sign_in_ip: "::1", confirmation_token: nil, 
+        confirmed_at: Time.now, confirmation_sent_at: nil, unconfirmed_email: nil, 
+        name: name, username: nil, role: 'musician'
+    )
 
-    Artist.create!(
-      name: artist,
-      user_id: user
-      )
+    Artist.find_by(name: artist).update_attributes(user_id: user.id)
 
   end
 
