52a44370d978d712bf3185a50402185ca99c296c
Gianpietro Lavado <gianpietro1@gmail.com>
2015-11-02 11:02:14 -0500

    facebook login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
     content_for :title, text
   end
   
+  def devise_mapping
+    Devise.mappings[:user]
+  end
+
+  def resource_name
+    devise_mapping.name
+  end
+
+  def resource_class
+    devise_mapping.to
+  end
+  
 end
