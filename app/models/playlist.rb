f92dc26dc180d0b1c6f963bea1ae892f2be7f1a2
Gianpietro Lavado <gianpietro1@gmail.com>
2015-11-03 00:03:27 -0500

    duration into playlists

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -5,4 +5,19 @@ class Playlist < ActiveRecord::Base
 
   mount_uploader :image, PlaylistUploader
 
+
+def length
+  @length = 0
+  self.songs.each do |song|
+    songpath = song.filename.file.file
+    TagLib::FileRef.open(songpath) do |fileref|
+      unless fileref.null?
+        properties = fileref.audio_properties
+        @length = @length + properties.length
+      end
+    end
+  end
+  return (@length/60).to_s
+end
+
 end
