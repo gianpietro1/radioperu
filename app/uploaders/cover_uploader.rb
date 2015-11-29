bc175b4634c2d6d85042d1c54ca33ff7034187a2
Gianpietro Lavado <gianpietro1@gmail.com>
2015-11-29 00:48:23 -0500

    8s for stats waiting for freeIP / avoid duplicate covers

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -48,8 +48,8 @@ class CoverUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "#{model.artist.name.to_s.split.join}-#{model.name.to_s.split.join}.jpg" if original_filename
+  end
 
 end
