a382698a10565aa3dd894800ccccd24db1c0412f
Gianpietro Lavado <gianpietro1@gmail.com>
2016-04-01 08:06:24 -0500

    omniauth google & homepage login partial dev

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -235,8 +235,8 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
   config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET']
-  config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET']
-
+  config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'], { }
+  
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
