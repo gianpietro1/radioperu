d63a93b9723018f81bfeef707c933d25c39aaf86
Gianpietro Lavado <gianpietro1@gmail.com>
2016-04-01 08:15:12 -0500

    omniauth google & homepage login

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,8 +1,15 @@
  class CallbacksController < Devise::OmniauthCallbacksController
     
     def facebook
-        @user = User.from_omniauth(request.env["omniauth.auth"])
-        sign_in_and_redirect @user
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      
+      if @user.persisted?
+        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+        sign_in_and_redirect @user, :event => :authentication
+      else
+        session["devise.google_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
     end
 
   	def google_oauth2
