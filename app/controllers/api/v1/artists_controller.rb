617c761b18d0f836cf11e2088f9ed93678d9ce3c
Gianpietro Lavado <gianpietro1@gmail.com>
2016-04-14 23:08:02 -0500

    logging for apps

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -9,6 +9,11 @@ class Api::V1::ArtistsController < Api::V1::BaseController
     # Pending: only log if Android / iPhone
     MyLog.log.info "Searching artist: " + params[:artist_name]
 
+    self.request.env.each do |header|
+      MyLog.log.warn "HEADER KEY: #{header[0]}"
+      MyLog.log.warn "HEADER VAL: #{header[1]}"
+    end
+
     render json: artist.to_json(:except => [ :created_at, :updated_at ]), status: 200
    end
  
