844b26bf9fd290d3ae1be35e8893df544bafa7ce
Gianpietro Lavado <gianpietro1@gmail.com>
2016-04-26 17:11:02 -0500

    change from sendgrid to mailgun

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,9 @@
 if Rails.env.development? || Rails.env.production? || Rails.env.staging?
     ActionMailer::Base.delivery_method = :smtp
     ActionMailer::Base.smtp_settings = {
-          address:        'smtp.sendgrid.net',
+          address:        'smtp.mailgun.org',
           port:           '587',
-          authentication: :plain,
-          user_name:      ENV['SENDGRID_USERNAME'],
-          password:       ENV['SENDGRID_PASSWORD'],
-          domain:         'heroku.com',
-          enable_starttls_auto: true
+          user_name:      ENV['MAILGUN_USERNAME'],
+          password:       ENV['MAILGUN_PASSWORD'],
     }
 end
