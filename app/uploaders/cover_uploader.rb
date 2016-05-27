673f2216b9aff6bf6cd7622fbea4e94c82bc9adb
Gianpietro Lavado <gianpietro1@gmail.com>
2016-05-26 20:54:10 -0500

    upscaling artist images

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -25,7 +25,7 @@ class CoverUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fill => [300,300]
+  process :resize_to_fill => [472,472]
   #
   # def scale(width, height)
   #   # do something
