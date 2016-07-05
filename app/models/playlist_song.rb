8530851b2edede89eeae0878405ff8147a885bf7
Gianpietro Lavado <gianpietro1@gmail.com>
2016-07-04 21:50:06 -0500

    top songs in playlists > 4

--- app/models/playlist_song.rb
+++ app/models/playlist_song.rb
@@ -3,7 +3,7 @@ class PlaylistSong < ActiveRecord::Base
   belongs_to :song
 
   def self.playlist_top_songs
-    sql = 'select * from (select song_id, count(song_id) as counts from playlist_songs group by song_id order by count(song_id) desc) as e where counts >=3'
+    sql = 'select * from (select song_id, count(song_id) as counts from playlist_songs group by song_id order by count(song_id) desc) as e where counts >=4'
     records_array = ActiveRecord::Base.connection.execute(sql)
     return records_array
   end
